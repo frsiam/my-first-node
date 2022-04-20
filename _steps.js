a7d45e4ddaa109688ff5618e3c7ab01e6e146183
frsiam <siam.frs@gmail.com>
2022-04-21 01:27:32 +0600

    add some steps

--- _steps.js
+++ _steps.js
@@ -1,4 +1,7 @@
 /*** 
+ * -------------------
+ * Basic setup
+ * -------------------
  * 1. create a folder (manually or mkdir)
  * 2. Open command line ot that folder 
  * 3. npm init ....... oR npm init -y(dile shobkisur answare deya hoye jabe)
@@ -16,7 +19,15 @@
  * 7. node index.js
  * 8. nodemon index.js (for live)
  * 9. data client site theke post korte chaile fetch methodeo post kora  jay *    sheta google kore nite hobe
- * 10. data post from client form clien and when we want to get from client * *    then we should body parse
+ * 10. data post from client form clien and when we want to get from client *     then we should body parse
+ * 11. check your browser for localhost for the 5000 port
+ * --------------
+ * get
+ * --------------
+ * 1. cors midlrware
+ * --------------
+ *  Post
+ * --------------
  * 
- * check your browser for localhost for the 5000 port
+ * 
 */
