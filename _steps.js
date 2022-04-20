66c48460464938caf0f917d0a7252228f9c7b8f9
frsiam <siam.frs@gmail.com>
2022-04-20 17:31:46 +0600

    initialize the first node project

---
+++ _steps.js
@@ -0,0 +1,11 @@
+/*** 
+ * 1. create a folder (manually or mkdir)
+ * 2. Open command line ot that folder 
+ * 3. npm init
+ * 4. npm install express
+ * 5. create index.js file
+ * --------------------------
+ *  For Express/node server
+ * --------------------------
+ * 5. 
+*/
