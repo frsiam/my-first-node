66165299edad3f2d462eefa5fb8da075d4548025
frsiam <siam.frs@gmail.com>
2022-04-23 01:24:18 +0600

    add array of object

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World ! Next task is nodemon installed')
 })
 
+const users = [
+    { name: 'bashar', id: 1, phone: 01236347 },
+    { name: 'ashar', id: 2, phone: 01231222 },
+    { name: 'basir', id: 3, phone: 01266347 }
+];
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+
+app.get('/user/:id', (req, res) => {
+    console.log(req.params);
+    const id = parseInt(req.params.id);
+    const user = users.find(u => u.id === id)
+    res.send(user);
+})
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`, port);
 })  
