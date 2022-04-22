22e2a717b282849080369ebbdce49afffdefcf18
frsiam <siam.frs@gmail.com>
2022-04-23 02:45:54 +0600

    update the data

--- _steps.js
+++ _steps.js
@@ -1,11 +1,33 @@
 /*** 
+ * -------------------
+ * Basic setup
+ * -------------------
  * 1. create a folder (manually or mkdir)
  * 2. Open command line ot that folder 
- * 3. npm init
- * 4. npm install express
- * 5. create index.js file
+ * 3. npm init ....... oR npm init -y(dile shobkisur answare deya hoye jabe)
+ * 4. create index.js file
+ * 5. npm install express
  * --------------------------
  *  For Express/node server
  * --------------------------
- * 5. 
+ * 1. require express
+ * 2. create app variable
+ * 3. create cors variable to require(cors)
+ * 4. declare port 
+ * 5. set app.get('/')
+ * 6. listen to port
+ * 7. node index.js
+ * 8. nodemon index.js (for live)
+ * 9. data client site theke post korte chaile fetch methodeo post kora  jay *    sheta google kore nite hobe
+ * 10. data post from client form clien and when we want to get from client *     then we should body parse
+ * 11. check your browser for localhost for the 5000 port
+ * --------------
+ * get
+ * --------------
+ * 1. cors midlrware
+ * --------------
+ *  Post
+ * --------------
+ * 
+ * 
 */
--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World ! Next task is nodemon installed')
-})
+app.use(cors());
+app.use(express.json());
 
 const users = [
-    { name: 'bashar', id: 1, phone: 01236347 },
-    { name: 'ashar', id: 2, phone: 01231222 },
-    { name: 'basir', id: 3, phone: 01266347 }
-];
+    { id: 1, name: 'Sabana', email: 'sabana@gmail.com', phone: "01239486754" },
+    { id: 2, name: 'Kobori', email: 'Kobori@gmail.com', phone: "01239486754" },
+    { id: 3, name: 'Nurani', email: 'Nurani@gmail.com', phone: "01239486754" },
+    { id: 4, name: 'Bobita', email: 'Bobita@gmail.com', phone: "01239486754" },
+    { id: 5, name: 'Razzak', email: 'Razzak@gmail.com', phone: "01239486754" }
+]
+
 app.get('/users', (req, res) => {
+    // filter by search query parameter
+    if (req.query.name) {
+        const search = req.query.name.toLowerCase();
+        const matched = users.filter(user => user.name.toLowerCase().includes(search));
+        res.send(matched);
+    }
+    console.log('query', req.query)
     res.send(users)
 })
 
 app.get('/user/:id', (req, res) => {
     console.log(req.params);
     const id = parseInt(req.params.id);
-    const user = users.find(u => u.id === id)
+    const user = users.find(u => u.id === id);
     res.send(user);
 })
+
+app.post('/user', (req, res) => {
+    console.log('request', req.body);
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    res.send(user)
+})
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`, port);
+    console.log(`Hello are you listening me from laptop on port`, port)
 })  
